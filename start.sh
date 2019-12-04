cf430c40a74d61e49f0308ce10e87f6922cba8ae
ramontiveros <rontiveros@sitek.mx>
2019-12-04 13:30:25 -0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ $# -eq 4 ]; then
+    if [ $4 = "F" ]; then
+       docker run --name $1 -i -t -p $2:8080 -v $3:/usr/local/tomcat/webapps/birt/reports cdorde/mybirt
+    else
+       if [ $4 = "D" ]; then
+          docker run --name $1 -d --restart=always -i -t -p $2:8080 -v $3:/usr/local/tomcat/webapps/birt/reports cdorde/mybirt
+       else
+          echo "wrong syntax. You must provide four parameters"
+          echo "sh start.sh <Name of container> <Port where tomcat will server Birt> <Path to reports directory> <(F)oreground or (D)aemon mode"
+          echo "sh start.sh BIRT001 9090 /root/birt_reports D"
+       fi 
+    fi
+else
+    echo "wrong syntax. You must provide four parameters"
+    echo "sh start.sh <Name of container> <Port where tomcat will server Birt> <Path to reports directory> <(F)oreground or (D)aemon mode"
+    echo "sh start.sh BIRT001 9090 /root/birt_reports D"
+fi
